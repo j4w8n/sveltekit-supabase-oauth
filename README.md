4354c7ae493cf022ea85d6a553998ba8698ff0b4
Jason Creviston <jcreviston@pm.me>
2022-05-12 13:21:19 -0400

    stuff

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# sveltekit + supabase with oauth
+# sveltekit + supabase oauth
+
+work in progress
+
+## notable features
+
+- event.locals - securely stores the access_token for server-side supabase db requests
+- shadow endpoints - instead of exporting a `load` function in `<script context="module">`
+- store - for reactivity of showing/hiding database info
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,4 +1,8 @@
 <script context="module">
+  /*
+  ** since shadow endpoints aren't currently supported for __layout.svelte,
+  ** we still need to use the load function here to access the sveltekit session store
+  */
   export const load = async ({ session }) => {
     return {
       props: {
@@ -23,10 +27,14 @@
       })
       .then((res) => {
         if (res.status === 200) {
-          // hydrate page data
+          // hydrate our page array
           loadPages()
 
-          // hydrate the sveltekit session store with data returned from supabase session
+          /* 
+          ** hydrate the sveltekit session store with data returned from supabase session.
+          ** note that $session = sesh would dangerously save the user's access_token in the session store
+          ** so only grab what you need
+          */
           $session = {
             user: {
               user_metadata: {
@@ -35,7 +43,12 @@
             }
           }
 
-          // fixes trailing hash issue with linkclick-and-backbutton navigation
+          /*
+          ** fixes trailing hash issue with linkclick-and-backbutton navigation
+          ** if removed, once you login there will be a `#` at the end of your url
+          ** then if you click to another route, say /app, everything works
+          ** but if you then click the browser's back button, the url changes but /app's content is still visible
+          */
           goto('/')
         } else {
           console.error('Failed to set cookie', res)
