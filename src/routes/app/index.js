005db38b9e9e0eea3370d556d546360260dabceb
Jason Creviston <jcreviston@pm.me>
2022-07-26 01:23:21 -0400

    moves  doc to main index.js

--- src/routes/app/index.js
+++ src/routes/app/index.js
@@ -1,11 +1,5 @@
 import { supabase } from '$lib/supabase'
 
-/*
-** Feel free to use GET({ locals }) instead of GET(event).
-** If you do this, then you'd remove 'event' from your 'locals.session' declaration.
-** I'm using event, to clarify that our session info here is coming
-** from what we set in the hooks.js handle function with event.locals.session
-*/
 export async function GET(event) {
   let svrPages
   const session = event.locals.session
