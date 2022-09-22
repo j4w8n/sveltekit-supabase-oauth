f72a81e6b4a849ef533eee414b1d41d2e502f90d
Jason Creviston <jcreviston@pm.me>
2022-09-22 06:52:14 -0400

    removes irrelevant routes. closes #1, closes #2
    Since this is a repo about authentication, I've removed the extra
    logic and routes which are specific to my supabase instance.
    
    With this update, if you're not authenticated, going to
    /app will redirect you to /login.

--- src/routes/(auth)/page/[page]/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-  export let data
-</script>
-
-{#if data.page}
-  <h1 class="h3">{data.page.subdomain}</h1>
-{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,2 @@
 <h1>HOME</h1>
 <a href="/app">App</a>
-<a href="/test">Test</a>
